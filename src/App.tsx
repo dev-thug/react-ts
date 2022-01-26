681de00e374ed10af0356a1b879442f871431305
HJ <de0978@gmail.com>
2022-01-26 15:48:15 +0900

    add state management using context-api

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,14 @@
 import Todos from "./components/Todos";
-import './App.css';
-import Todo from "./models/todo";
 import NewTodo from "./components/NewTodo";
-import {useState} from "react";
+import TodosContextProvider from "./store/todos-context";
 
 function App() {
 
-    const [todos, setTodos] = useState<Todo[]>([])
-
-    const addTodoHandler = (todoText: string) => {
-        const newTodo = new Todo(todoText);
-        setTodos(prevState => {
-            return prevState.concat(newTodo);
-        })
-    };
-
-    const removeTodoHandler = (todoId: string) => {
-        setTodos(prevState => {
-            return prevState.filter(todo => todo.id !== todoId);
-        })
-    };
-
     return (
-        <div>
-            <NewTodo onAddTodo={addTodoHandler}/>
-            <Todos items={todos} onRemoveTodo={removeTodoHandler}/>
-        </div>
+        <TodosContextProvider>
+            <NewTodo/>
+            <Todos/>
+        </TodosContextProvider>
     );
 }
 
